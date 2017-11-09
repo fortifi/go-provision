4f635cba7dad8967340b433da8396f71e57f5160
Brooke Bryan <brooke@bajb.net>
2017-11-09 16:45:19 +0000

    Provisioning Type

---
+++ provision.go
@@ -0,0 +1,48 @@
+package provisioning
+
+import "time"
+
+func Time() int64 {
+	return time.Now().UnixNano() / int64(time.Millisecond)
+}
+
+type BaseTransport struct {
+	Timestamp       int64               `json:"timestamp"`
+	CustomerFid     string              `json:"customerFid"`
+	SubscriptionFid string              `json:"subscriptionFid"`
+	Properties      []TransportProperty `json:"properties"`
+}
+
+type Response struct {
+	BaseTransport
+	Type    string       `json:"type"`
+	Message string       `json:"message"`
+	Log     []LogMessage `json:"log"`
+}
+
+type Request struct {
+	BaseTransport
+	Type           string            `json:"type"`
+	OrderFid       string            `json:"orderFid"`
+	ProductFid     string            `json:"productFid"`
+	PriceFid       string            `json:"priceFid"`
+	StartTimestamp int64             `json:"startTimestamp"`
+	RenewTimestamp int64             `json:"renewTimestamp"`
+	EndTimestamp   int64             `json:"endTimestamp"`
+	Configuration  map[string]string `json:"configuration"`
+	Cycle          string            `json:"cycle"`
+}
+
+type LogMessage struct {
+	Timestamp int64  `json:"timestamp"`
+	Type      string `json:"type"`
+	Message   string `json:"message"`
+}
+
+type TransportProperty struct {
+	Key         string `json:"key"`
+	Type        string `json:"type"`
+	StringValue string `json:"stringValue"`
+	FlagValue   bool   `json:"flagValue"`
+	CountValue  int64  `json:"countValue"`
+}
